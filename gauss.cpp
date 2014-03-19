ea7954b32d4dcea31914425a580f309894e7b9e7
Maciej Chudak <macioosch@gmail.com>
2014-03-20 00:22:02 +0100

    IT WORKS

--- gauss.cpp
+++ gauss.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void swap_arrays(double r1[], double r2[], int n)
@@ -14,6 +15,7 @@ void swap_arrays(double r1[], double r2[], int n)
 int gauss(double **t, int m, int n, double *w)
 {
     for (int k=0; k<m; ++k) {
+        // increase stability by some row-swapping
         int i_max = k;
         double max_square = t[i_max][k]*t[i_max][k];
         for (int i=k+1; i<m; ++i) // 1 not 0 on purpose
@@ -24,12 +26,21 @@ int gauss(double **t, int m, int n, double *w)
         if (t[i_max][k] == 0)
             return -1;
         swap_arrays(t[k], t[i_max], n);
+
         for (int i=k+1; i<m; ++i) {
+            double multiplier = t[i][k] / t[k][k];
             for (int j=k; j<n; ++j)
-                t[i][j] = t[i][j] - t[k][j] * t[i][k] / t[k][k];
+                t[i][j] -= t[k][j] * multiplier;
             t[i][k] = 0;
         }
     }
+
+    for (int i=m-1; i>=0; --i) {
+        w[i] = t[i][n-1] / t[i][i];
+        for (int j=n-2; j>i; --j)
+            w[i] -= w[j] * t[i][j] / t[i][i];
+    }
+
     return 0;
 }
 
@@ -43,8 +54,10 @@ int main()
     for (int i=0; i<m; ++i)
         t[i] = new double[n];
 
-    t[0][0]=1; t[0][1]=1; t[0][2]=1;
-    t[1][0]=3; t[1][1]=1; t[1][2]=2;
+    t[0][0]=-1; t[0][1]= 1; t[0][2]=-1;
+    t[1][0]=-2; t[1][1]= 1; t[1][2]= 2;
+
+    cout << setprecision(4);
 
     gauss(t, m, n, w);
 
